34d748e32bb35d839ce97cd7d1d281be69b5bcca
Jesus Posso <jposso@joonik.com>
2022-11-22 15:20:58 -0500

    fix: change port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config()
 require('colors')
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.use(express.json());
 
